2333e02c8262b293f0bf345c2c96351a0533d54b
Stephan Hochhaus <stephan@yauh.de>
2015-04-27 10:39:51 +0200

    add server method to illustrate debugging

---
+++ loadOrder/server/methods.js
@@ -0,0 +1,13 @@
+Meteor.methods({
+  // debugMethod
+  'debugMethod': function () {
+    console.log('Method.debugMethod called');
+    debugger;
+    var value1 = 1;
+    var value2 = 2;
+    debugger;
+    var sum = value1 + value2;
+    console.log('sum is ' + sum)
+    return sum;
+  }
+});
