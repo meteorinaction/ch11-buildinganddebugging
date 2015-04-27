5063246c6b765812c42f920968a4ad8e99700299
Stephan Hochhaus <stephan@yauh.de>
2015-04-27 11:37:46 +0200

    Listing 11.7 Sample App.info() for a mobile app

---
+++ mobileApp/mobile-config.js
@@ -0,0 +1,10 @@
+// Set up app metadata
+App.info({
+  id: 'com.meteorinaction.mobile.app',
+  version: '1.0.0',
+  name: 'Meteor in Action Mobile App',
+  description: 'This is a mobile app for Meteor in Action',
+  author: 'Stephan Hochhaus',
+  email: 'stephan@meteorinaction.com',
+  website: 'http://meteorinaction.com'
+});
