c8c695f99fb2da36abbe44dec1c7279f796cd002
Stephan Hochhaus <stephan@yauh.de>
2015-04-27 12:15:16 +0200

    Listing 11.11 Adding the Cordova dialogs plug-in to a change event

--- mobileApp/client/client.js
+++ mobileApp/client/client.js
@@ -7,3 +7,23 @@ Template.location.helpers({
     return Geolocation.currentLocation();
   }
 });
+
+Template.select.events({
+  'change #platform': function (evt) {
+    var selectedPlatform = evt.currentTarget.value;
+    if (Meteor.isCordova) {
+      navigator.notification.alert(
+        'You picked ' + selectedPlatform, // message
+        null, // callback
+        'Your choice', // title
+        'I know' // buttonName
+      );
+      navigator.notification.alert(
+        'You selected',
+        selectedPlatform
+      );
+    } else {
+      console.log('selected ' + selectedPlatform)
+    }
+  }
+});
